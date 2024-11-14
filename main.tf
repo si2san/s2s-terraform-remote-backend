ec419d1dc289a38d783e647aa3cbf666306bcf26
sithusan <sithusan@hivelocity.co.jp>
2024-11-15 00:00:20 +0700

    feat: add s3 properties

--- main.tf
+++ main.tf
@@ -1,3 +1,36 @@
 resource "aws_s3_bucket" "sts_aws_master" {
   bucket = var.s3_bucket_name
 }
+
+resource "aws_s3_bucket_ownership_controls" "s3_ownership" {
+  bucket = aws_s3_bucket.sts_aws_master.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "s3_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.s3_ownership]
+  bucket     = aws_s3_bucket.sts_aws_master.id
+  acl        = "private"
+}
+
+resource "aws_s3_bucket_versioning" "s3_versioning" {
+  bucket = aws_s3_bucket.sts_aws_master.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_server_side_encryption" {
+  bucket = aws_s3_bucket.sts_aws_master.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+
